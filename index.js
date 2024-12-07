ca9bd5aa946737c3714ae5a4deefbc5b26fb63e5
Harrison-Holt <hholt2901@gmail.com>
2024-12-07 18:03:48 -0500

    Initial Commit

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ async function get_quote() {
         }
 
         const data = await response.json(); 
-        console.log(data.quote); 
+        console.log(data); 
 
         quote_container.innerHTML = `"${data.quote}" <br>
                                         - ${data.author}`; 
